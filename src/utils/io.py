b34a889bc639c071ef2494f0491fc62d9a3510ae
SathvikY3533 <sathvik.y007@gmail.com>
2024-07-30 11:20:43 -0500

    init

--- src/utils/io.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-def csv_to_list(csv_path):
-    as_list = None
-    with open(csv_path, 'r') as f:
-        reader = csv.reader(f)
-        as_list = list(reader)
-    return as_list
-
